a3fad3caee9fb6c493966df59ce1abba5760bb72
Ayodhya <ayodhyarode03>
2025-08-13 10:37:07 +0530

    Array.js

---
+++ Array.js
@@ -0,0 +1,126 @@
+//INITILIZING ARRAY
+
+// let arr = [1,2,3,4,5]
+// console.log(arr[0] +10);
+// const one = arr[0] +100
+// console.log(one);
+
+// let arr = [10]
+// arr[12] = 33     //will add 33 to 12th posiotion and others are emptry
+
+// let arr2 = new Array(10).fill(2)
+// console.log(arr);
+// console.log(arr2);
+// console.log(arr2.length);
+
+// let arr = [10].fill(2)
+// arr[12] = 33 
+// console.log(arr);
+
+// CONVERTING ARRAY TO STRING
+
+// let arr = [1,2,3,4,5]
+// console.log(typeof (arr));
+// console.log(arr.toString());
+// console.log(typeof (arr[1]));
+
+// ACESSING ELEMETS
+
+// let arr = [1,2,3,4,5]
+// console.log(arr[1])
+// console.log(arr[arr.length-1])
+
+// let str = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(str[0]);
+// console.log(str[str.length-1]);
+
+
+//ADDING ELEMENT
+
+// let arr =[1,2,3]
+// console.log(arr.push(23))
+// console.log(arr);
+
+// console.log(arr.unshift(100))
+// console.log(arr);
+// console.log('2nd');
+
+// let arr2 =[1,2,3,4,5,6,7]
+// // console.log(arr2.pop());
+// // console.log(arr2);
+// console.log(arr.shift());
+// console.log(arr);
+
+
+// LENGHT OF ARRAY
+
+// let arr = [1,2,3,4,95,0]
+// console.log(arr.length);
+// console.log('new');
+// arr.length =2
+// console.log(arr.length);
+// console.log(arr);
+
+// let arr = [1,2,3]
+// let arr2 = arr
+// arr[3]=67
+
+// console.log(arr === arr2);
+
+
+// AT()
+
+// let arr = [1,2,3,4,5,7,6]
+// console.log(arr[1]);
+// console.log(arr.at(1));
+
+// JOIN()
+
+//  let arr = [1,2,3,4,5,7,6]
+//  console.log(typeof arr);
+  
+//  console.log(arr.join('*'));
+//   console.log(arr);
+
+// CONCAT()
+
+// let a = [5,3,6,6]
+// let b = [0,9,8,2,1]
+// let c = [99,33,44]
+// console.log(typeof a);
+
+// let d = a.concat(b,c)
+// console.log(typeof d);
+// console.log(typeof a);
+
+
+// console.log(a==d);
+
+// console.log(typeof d);
+// console.log(a);
+
+// copyWithin()
+
+// let a = [9,8,3,1,6]
+// let b = a.copyWithin(0,2)
+// console.log(b); //[3,1,6,1,6]
+// console.log(a);
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango","Pineapple"];
+// console.log(typeof fruits);
+
+// const a = fruits.toString()
+// console.log(typeof a);
+
+// console.log(a);
+
+// const modifyed = fruits.copyWithin(1,3)  
+// console.log(modifyed);
+
+// const myArr = [[1,2],[3,4],[5,6,[9,9,9,[7,7,77]]]];
+// const newArr = myArr.flat(Infinity);
+// console.log(newArr);
+
+// console.log(myArr);
+
