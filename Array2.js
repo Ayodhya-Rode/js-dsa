75139c0802a9521d5fad0ac8e57f6ff84b69c171
Ayodhya <ayodhyarode03>
2025-08-14 10:32:08 +0530

    Array2.js

---
+++ Array2.js
@@ -0,0 +1,94 @@
+// MAP()
+
+// let arr = [4,7,2,7,9,4,5]
+
+// let result = arr.map((ele)=>{
+//     return(ele);
+    
+// })
+// console.log(result);
+
+
+// console.log(arr.map((ele)=> ele+2))
+// console.log(arr);
+
+// FILTER()
+
+// let arr = [4,7,2,7,9,4,5]
+// console.log(arr.filter((ele)=>ele%2==0))
+
+// let result = arr.filter((ele) => ele*2==8)
+// console.log(result);
+//  console.log(arr);
+
+
+// REDUCE()
+
+// let arr =[8,5]
+
+// function add(result , current){
+//     return result + current
+// }
+
+// let sum = arr.reduce(add,10)
+// console.log(sum);
+
+
+// let result = arr.reduce((result, current)=>{
+//     return result+ current
+// },0)
+// console.log(result);
+
+
+// FOREACH()
+
+// let arr = [4,7,2,7,9,4,5]
+
+// let r = arr.map(ele => ele+2);
+// console.log("map is",r);
+
+// let s = arr.forEach(ele =>{
+//     console.log("each is",ele +2);
+    
+// });
+
+
+// FIND()
+
+// let arr = [4,7,2,7,9,4,5]
+// let r = arr.find(ele =>ele>0)
+// console.log(r);
+
+
+// INCLUDES()
+
+// let arr = [4, 7, 2, 9, 4, 5];
+
+// console.log(arr.includes(7));   // true
+// console.log(arr.includes(10));  // false
+// console.log(arr.includes(7, 2)); // true (starts from index 2)
+
+//INDEXOF()
+// let arr = [4,2, 7, 2, 9, 4, 5];
+// console.log(arr.indexOf(2));
+
+// // SLICE()
+// let arr = [4, 7, 2, 9, 4, 5];
+ 
+// console.log(arr.slice(1,5));
+
+// console.log(arr);
+
+
+//SPLICE()
+
+// let arr = [1,2,3,4]
+// arr.splice(4,0,5,6,7,8);
+// arr.splice(0,2,9,8,7)
+// console.log(arr);
+
+
+//REVERSE()
+// let arr = [1,2,3,4,5,6]
+// let r = arr.splice(1,4).reverse()
+// console.log(r);
