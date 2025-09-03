39b311153f2c5650fb414a10cd0f6cf0f20bd43b
Ayodhya <ayodhyarode03>
2025-09-03 18:03:04 +0530

    updating 01_timeAndSpace.js

--- 01_timeAndSpace.js
+++ 01_timeAndSpace.js
@@ -16,9 +16,27 @@
 
 
 // O(1)     O of 1
+
 // const groceryItems = ["Rice","Wheat Flour","Sugar","Milk","Eggs","Vegetables","Fruits","Cooking Oil","Salt","Tea Powder"];
 
 // function findSpecific(array, index){
 //     console.log(array[index])
 // }
 // findSpecific(groceryItems,9)
+
+
+
+// o(n^2)   O of n square
+
+// const numbers = [1,2,3,4,5,6,7,8,9];
+
+// function number(num){
+//     for (let i = 0; i < num.length; i++) {
+//         console.log(`iteration on ${num[i]}`)
+//         for(let j = 1; j<num.length;j++){
+//             console.log(`pairs ${num[i]} - ${num[j]}`);
+            
+//         }        
+//     }
+// }
+// number(numbers)
